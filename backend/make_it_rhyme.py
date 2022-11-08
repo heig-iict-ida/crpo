339128f8122763789ff55fe5a3f2952cfcaa5679
Andrei Popescu-Belis <andreipb@users.noreply.github.com>
2022-11-08 18:44:16 +0100

    Accelerate generation of rhymes
    Only one access to the pronunciation file, some loops improved.

--- backend/make_it_rhyme.py
+++ backend/make_it_rhyme.py
@@ -20,19 +20,16 @@ from backend.generer_poesie import flatten
 
 
 def get_rhyming_array (path=conf.RIME_PICKLE_FILE):
-
     pickle_in = open(path,"rb")
     warr = pickle.load(pickle_in)
     warr = np.array(warr, dtype=object)
     return warr
-
 
 def tokenize_poem (text):
     """
     Tokenize poem into strophes and verses.
     """
     structured = []
-
     strophes = text.split('\n\n')
     for s in strophes:
         strophe = []
@@ -40,7 +37,6 @@ def tokenize_poem (text):
         for v in verses:
             strophe += [v]
         structured += [strophe]
-
     return structured
 
 
@@ -50,7 +46,7 @@ def process_verse (verse):
     of verse without either of the two.
     """
 
-    punctuation = ['.', ',', ',', ':', ';', '!', '?', ' ']
+    punctuation = ['.', ',', ',', ':', ';', '!', '?', ' ', '-']
 
     tok_verse = word_tokenize(verse, language='english') #changed to english -Teo
     # Remember punctuation
@@ -78,7 +74,7 @@ def process_verse (verse):
         pass
 
     new_verse = ' '.join(tok_verse[:-1])
-    new_verse = re.sub (r" ’ ",r'’', new_verse)
+    new_verse = re.sub (r" â€™ ",r'â€™', new_verse)
     new_verse = re.sub (r" \.", '.', new_verse)
 
     return original_punctuation, last_word, new_verse
@@ -88,37 +84,27 @@ def get_candidates_rime (word_to_replace, word, arr, all_phonemes, same_pos=True
     """
     Given a word and an array, returns its pronunciation according to the array
     and a list of candidate words that have the same pronunciation.
-
     same_pos = whether we check for candidates with exclusively the same POS.
-
     n_syllable = how many of the syllables we count; n=1 means we just look at the last
     vowel in the pronunciation.
     """
-
-#     uppercase = False
-#     if word[0] != word[0].lower():
-#         uppercase = True
-
     word = word.lower()
+    word_in_array = np.where(arr[:,0] == word.lower()) # index of the word in array
+    # print('word= {}, word_in_array = {}, len(arr)= {}'.format(word, word_in_array, len(arr)))
 
-    word_in_array = np.where(arr[:,0] == word.lower())
-
-    if (np.asarray(word_in_array)).shape[1] == 0:
+    if (np.asarray(word_in_array)).shape[1] == 0: # if word not in pronunciation dictionary, 
+        # find similar words (better test: "if word_in_array[0].size == 0")
         possibles = difflib.get_close_matches(word, arr[:,0])
         word_in_array = np.where(arr[:,0] == possibles[0].lower())
-#         print('found similar word:', word, possibles[0])
 
-    original = np.asarray(word_in_array)[0][0]
-    word, rime, pos = arr[original]
+    original = np.asarray(word_in_array)[0][0] # index of the word or a similar one
+    word, rime, pos = arr[original] # there are 29890 out of 370099 words with POS!='unknown'
 
     if n_syllable != 'all':
-        rime = rime[-n_syllable:] #TODO syllables to phonemes
-
-#     print('WORD:', word, '|', 'RIME:', rime, '|','POS:', pos)
+        rime = rime[-n_syllable:] #TODO: syllables to phonemes
 
     if  len(rime) < n_syllable :
-
-        all_phonemes = get_rhyming_array()[:,1]
+        all_phonemes = arr[:,1]
 
         nb_phonems = min(n_syllable,len(rime))
         for i in range(len(all_phonemes)):
@@ -130,11 +116,8 @@ def get_candidates_rime (word_to_replace, word, arr, all_phonemes, same_pos=True
     joined_rime = "".join(rime)
 
     if same_pos == True:
-#         print('Same POS')
         word_to_replace = word_to_replace.lower()
-
         word_in_array = np.where(arr[:,0] == word_to_replace.lower())
-
         if (np.asarray(word_in_array)).shape[1] == 0:
             possibles = difflib.get_close_matches(word_to_replace, arr[:,0])
             word_in_array = np.where(arr[:,0] == possibles[0].lower())
@@ -143,27 +126,19 @@ def get_candidates_rime (word_to_replace, word, arr, all_phonemes, same_pos=True
         original = np.asarray(word_in_array)[0][0]
         word_to_replace, rime_to_replace, pos_to_replace = arr[original]
 
-        idxs = np.asarray(np.where((all_phonemes == joined_rime) & (arr[:,2] == pos_to_replace)))
-    else:
-#         print('Any POS')
-        idxs = np.asarray(np.where((all_phonemes == joined_rime)))
-        
+        idxs = np.asarray(np.where((np.array(all_phonemes) == joined_rime) & (arr[:,2] == pos_to_replace)))
+    else: # any POS
+        idxs = np.asarray(np.where((np.array(all_phonemes) == joined_rime))) # must convert to np.array, if not: empty result!
 
     candidates = (arr[idxs][0][:,0]).tolist()
-
-#     if uppercase:
-#         candidates = [i[0].upper() + i[1:] for i in candidates]
-
     candidates = [c for c in candidates if c.lower() != word.lower()]
-
     return rime, candidates
 
-def seq_prob (verse_idx, strophe, old_verse, candidate,
-             models):
+
+def seq_prob (verse_idx, strophe, old_verse, candidate, models):
     """
     Calculates probability of a sequence of old_verse + ending (candidate) word.
     """
-   
     candidate_in_context = []
     for i in range(len(strophe)):
         candidate_in_context.append("<start>" + strophe[i])
@@ -174,8 +149,7 @@ def seq_prob (verse_idx, strophe, old_verse, candidate,
     loss = models[1](tensor_input, labels=tensor_input)
     return -loss[0].item()
 
-#print_into_file may be used to DEBUG ONLY (save to disk before and after rhyme)
-#d allows printing of detailed debug info in TERMINAL
+
 def apply_rhyming_scheme (poem,
                           models,
                           rhyming_scheme,
@@ -187,85 +161,68 @@ def apply_rhyming_scheme (poem,
     """
     Gets a poem raw (as the first stage generates it), and according to the rhyming scheme
     (e.g., ABBA ABBA CCD EDE, etc. [which is now missing]), returns it rhyming.
-
     n_candidates = for how many candidate words are we going to calculate their probability.
-
-    rime_syllables = number of syllables that should rhyme
+    rime_syllables = number of syllables that should rhyme.
+    print_into_file is used to DEBUG only (save to disk before and after rhyme)
+    Morever, 'd' allows printing of detailed debug info in TERMINAL
     """
-
-    punctuation = ['.', ',', ',', ':', ';', '!', '?', ' ']
-
-
-    tok_poem = tokenize_poem(poem)
+    punctuation = ['.', ',', ',', ':', ';', '!', '?', ' ', '-']
+    tok_poem = tokenize_poem(poem) # segment poem into stanzas and verses.
 
     new_poem = []
     new_poem_markup = []
-
     used_candidates = []
-
     strophes_scheme = rhyming_scheme.split(' ')
+    rhyming_array = get_rhyming_array() # 370099 words with their pronunciation
+    all_phonemes = rhyming_array[:,1] # from conf.RIME_PICKLE_FILE
 
-    all_phonemes = get_rhyming_array()[:,1]
+    #for i in range(len(all_phonemes)):
+    #    all_phonemes[i] = all_phonemes[i][-rime_syllables:]
+    #for i in range(len(all_phonemes)):
+    #    all_phonemes[i] = "".join(all_phonemes[i]) # too slow for 20 MB of data
+    all_phonemes = [''.join(ph[-rime_syllables:]) for ph in all_phonemes]
 
-    nb_phonems = rime_syllables
-    for i in range(len(all_phonemes)):
-        all_phonemes[i] = all_phonemes[i][-nb_phonems:]
-
-    for i in range(len(all_phonemes)):
-        all_phonemes[i] = "".join(all_phonemes[i])
-
-    all_rhymes = {}
-    for l in ''.join(strophes_scheme):
-        all_rhymes[l] = ''
+    # all_rhymes = {}      # seems to create a dictionary with entries for each of the rhymes 
+    #for l in ''.join(strophes_scheme):  # in the scheme ('ABBA CDDE' -> {'A':'', 'B':'', etc.)
+    #     all_rhymes[l] = '' # quite inefficient, even if short
+    all_rhymes = dict([(c, '') for c in set(rhyming_scheme) if not c == ' '])
 
     for strophe_idx in range(len(tok_poem)):
-
         strophe = tok_poem[strophe_idx]
         new_strophe = []
         new_strophe_markup =[]
-        if d: print('\nStrophe {}\n'.format(strophe_idx + 1))
 
         for verse_idx in range(len(strophe)):
-
             full_old_verse = strophe[verse_idx]
             all_probs = []
-
             punct, word, old_verse = process_verse(full_old_verse)
-#             if d: print('\t', punct, '|', word, '|', old_verse)
-            if d: print('V: {} {}'.format(strophe_idx, verse_idx))
-
             verse_scheme = strophes_scheme[strophe_idx][verse_idx]
+            if d: 
+                print('\tStanza: {} Verse: {} Rhyme: {}'.format(strophe_idx+1, verse_idx+1, verse_scheme))
+                print(all_rhymes[verse_scheme])
 
-            if d: print('\tVerse {}  {}'.format((verse_idx + 1), verse_scheme))
-
-
-            # If it is a new rhyme leave it as is
-            if all_rhymes[verse_scheme] == '':
-
+            if all_rhymes[verse_scheme] == '':  # If it is a new rhyme leave it as is
                 all_rhymes[verse_scheme] = word
                 final_word = word
                 final_word_markup = word
                 rime = 'nochange'
-
-            # Not a new rhyme
-            else:
-
+            else: # If it is not a new rhyme
                 rime, candidates = get_candidates_rime(word, all_rhymes[verse_scheme],
-                                                       get_rhyming_array(), all_phonemes,
-                                                       same_pos=False, # or True
+                                                       rhyming_array, all_phonemes,
+                                                       same_pos=True, # for the first call
                                                        n_syllable=rime_syllables)
-
-                candidates = [i for i in candidates if i not in used_candidates]
-                candidates = [i for i in candidates if len(i) > 1]
+                if d: print("#1a", candidates)
+                candidates = [i for i in candidates if i not in used_candidates and len(i) > 1]
+                if d: print("#1b", candidates)
 
                 if len(candidates) == 0:
                     rime, candidates = get_candidates_rime(word, all_rhymes[verse_scheme],
-                                                       get_rhyming_array(), all_phonemes,
-                                                       same_pos=False,
+                                                       rhyming_array, all_phonemes,
+                                                       same_pos=False, # if nothing found in the 1st call
                                                        n_syllable=rime_syllables)
                     
-                    candidates = [i for i in candidates if i not in used_candidates]
-                    candidates = [i for i in candidates if len(i) > 1]
+                    candidates = [i for i in candidates if i not in used_candidates and len(i) > 1]
+                    if d: print("#2", candidates)
 
                     if len(candidates) == 0:
                         candidates = [word]                       
@@ -276,18 +233,18 @@ def apply_rhyming_scheme (poem,
                 random.shuffle(candidates)
                 random_candidates = random.sample(candidates, n_candidates)
 
-                if d: print('\t\tALL CANDIDATES:', len(candidates), '\n')
-                if len(candidates) < 100:
-                    if d: print('\t\t', word, rime, candidates, '\n')
+                if d: 
+                    print('\t\tALL CANDIDATES:', len(candidates), '\n')
+                    if len(candidates) < 100:
+                        print('\t\t', word, rime, candidates, '\n')
 
                 for candidate in random_candidates:
-
-                    prob = seq_prob (verse_idx, strophe, old_verse, candidate,
-                             models)
-
+                    prob = seq_prob (verse_idx, strophe, old_verse, candidate, models) 
+                    # vu l'appel de apply_rhyming_scheme
+                    # on n'aurait ici que la pipeline avec les modÃ¨les
                     all_probs += [prob]
 
-                highest_to_lowest = np.argsort(all_probs)[::-1]
+                highest_to_lowest = np.argsort(all_probs)[::-1] # why sort all if we only need max?
                 best = highest_to_lowest[0]
                 best_prob = all_probs[best]
                 final_word = random_candidates[best]
@@ -296,11 +253,9 @@ def apply_rhyming_scheme (poem,
             if len(punct) != 0:
                 best_riming_verse = old_verse + ' ' + ''.join(final_word) + punct[0]
                 best_riming_verse_markup = old_verse + ' ' + ''.join(final_word_markup) + punct[0]
-                # best_riming_verse += ' | ' + verse_scheme + ' (' + rime + ')'
             else:
                 best_riming_verse = old_verse + ' ' + ''.join(final_word)
                 best_riming_verse_markup = old_verse + ' ' + ''.join(final_word_markup)
-                # best_riming_verse += ' | ' + verse_scheme + ' (' + rime + ')'
 
             used_candidates += [final_word]
 
@@ -322,20 +277,17 @@ def apply_rhyming_scheme (poem,
     raw_markup = ''.join((flatten(new_poem_markup)))
     new_poem_markup = postprocessing(raw_markup, punctuation)
 
-    both_poems = 'NEW POEM\n\n' + new_poem + '\n' + '~'*50 + '\n' + 'OLD POEM\n\n' + poem
-    if d: print(both_poems)
-
+    if d: 
+        print('NEW POEM\n\n' + new_poem + '\n' + '~'*50 + '\n' + 'OLD POEM\n\n' + poem)
     if print_into_file:
-
         now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S_RHYME") #Windows compatible
         with open(outfile_directory + now + '.txt', 'w') as f:
-            f.write(both_poems)
-
+            f.write('NEW POEM\n\n' + new_poem + '\n' + '~'*50 + '\n' + 'OLD POEM\n\n' + poem)
 
     return (new_poem_markup, new_poem)
 
 
-def make_it_rhyme   (poem,
+def make_it_rhyme(poem,
                      models,
                      rhyming_scheme,
                      outfile_directory=conf.OUTPUT_DIRECTORY_FILE,
@@ -345,19 +297,20 @@ def make_it_rhyme   (poem,
     Calls apply_rhyming_scheme() with correct scheme and model mixing %s parameters.
 
     models: list of list of built models, their vocabs, and their indices_chars.
+    
+    In reality, all arguments of make_it_rhyme are passed identically, except the model (pass only pipeline). 
     """
 
     return apply_rhyming_scheme(poem,
                                models[0],  # called from 'stage_4_rhyme.py' with only the general model (GPT-2)
+                                           # 'models' has [pipeline, model, tokenizer] so we keep only the pipeline (?)
                                rhyming_scheme,
                                outfile_directory,
                                n_candidates=n_candidates,
                                rime_syllables=rime_syllables)
 
 
-def get_poem (poem):
-
-    with open(poem, 'r') as p:
-        raw = p.read()
-
-    return raw
+#def get_poem (poem):
+#    with open(poem, 'r') as p:
+#        raw = p.read()
+#    return raw
